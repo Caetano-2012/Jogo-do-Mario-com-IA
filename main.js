9b39172d56dfb8eb9e8e5649b1cb863d1cc7dea7
Caetano Bordin <hyperplayer2012@gmail.com>
2025-10-27 14:53:57 -0300

    Atualização JS

--- main.js
+++ main.js
@@ -6,11 +6,31 @@ function preload() {
 
 function setup() {
 	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+
 	instializeInSetup(mario);
+
+	let video = createCapture(VIDEO);
+  	video.size(800, 400);
+	video.parent('game_console');
+
+	let poseNet = ml5.poseNet(video, modelLoaded);
+  	poseNet.on("pose", gotPoses);
 }
 
 function draw() {
 	game()
+}
+
+function gotPoses(results) {
+  if(results.length > 0) {
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+  }
+}
+
+function modelLoaded() {
+  console.log("Modelo inicializado!")
 }
 
 
